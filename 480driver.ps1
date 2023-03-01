426741c0038c7f126079e6351371fbfaf719b4ce
JackLeonard802 <jack.leonard@mymail.champlain.edu>
2023-03-01 00:19:31 -0500

    update 480utils

--- 480driver.ps1
+++ 480driver.ps1
@@ -1,11 +1,27 @@
+# import module
 Import-Module '480-utils' -Force
 
+# present banner
 480Banner
+
+# load json as conf
 $conf = Get-480Config -config_path "/home/jack/Documents/SYS-480/480.json"
+
+# Connect to vCenter
 480connect -server $conf.vcenter_server
+
+# Select VM from folder
 Write-Host "Selecting your VM"
 $selected_vm = Select-VM -folder "BASEVM"
+
+# Select snapshot
 $snapshot = vmSnapshot -snapshot_name $conf.snapshot -selected_vm $selected_vm
+
+# Select ESXi Host
 $vmhost = esxiHost -hostIP $conf.esxi_host
+
+# Select datastore
 $ds = datastoreName -datastore $conf.default_datastore
+
+# Create Linked Clone
 linkedClone -vm $selected_vm -snapshot $snapshot -vmhost $vmhost -ds $ds -net $conf.default_network
--- modules/480-utils/480-utils.psm1
+++ modules/480-utils/480-utils.psm1
@@ -1,4 +1,5 @@
 function 480Banner()
+# Presents Banner for 480Utils
 {
     $banner = @"
      __ __  ____  ____ 
@@ -17,56 +18,76 @@ function 480Banner()
 }
 
 function 480connect([string] $server)
+# connects to vCenter
 {
     $conn = $Global:DefaultVIServer
 
     if ($conn){
         $msg = "Already connected to: {0}" -f $conn
 
+        # Presents if user is already connected
         Write-Host -ForegroundColor Green $msg
     }else
     {
+        # Connecting to server
         $conn = Connect-VIServer -Server $server
     }
 }
 
 function Get-480Config([string] $config_path)
+# Pulls config file and converts data from json
 {
     $conf=$null
     if(Test-Path $config_path)
     {
+        # Converts from json
         $conf = (Get-Content -Raw -Path $config_path | ConvertFrom-Json)
         $msg = "Using Configuration at {0}" -f $config_path
         Write-Host -ForegroundColor Green $msg
     }else
     {
+        # Presents if no configuration present
         Write-Host -ForegroundColor Yellow "No Configuration"
     }
+
+    # Returns config
     return $conf
 }
 
 function Select-VM([string] $folder)
+# Presents VM selection Menu and allows user to select VM
 {
+    # Clears selection
     $selected_vm=$null
+
+    # Error handling
     try
     {
+        # Gets VMs from folder
         $vms = Get-VM -Location $folder
         $index = 1
         foreach($vm in $vms)
         {
+            #Lists each VM in folder
             Write-Host [$index] $vm.name
             $index+=1
         }
+
+        # Prompts user to pick VM from index number
         $pick_index =  Read-Host "Which index number [x] do you wish to pick?"
-        # 480-TODO need to deal with an invalid index (consider making this check a fuction)
+
+        # Handles invalid index
         if ($pick_index -gt $index -1)
         {
+            # Notifies user of invalid index
             Write-Host "Please select a valid VM" -ForegroundColor Yellow
         }else 
         {
+            # Selects VM that user specified
             $selected_vm = $vms[$pick_index -1]
             Write-Host "You picked " $selected_vm.Name
-            # note this is a full on vm object that we can interact with
+            
+            # Returns VM object
             return $selected_vm
         }
 
@@ -74,24 +95,33 @@ function Select-VM([string] $folder)
     }
     catch
     {
+        # Displays if folder is invalid
         Write-Host "Invalid Folder: $folder" -ForegroundColor Red
     }
 }
 
 function vmSnapshot ([string] $snapshot_name, $selected_vm)
+# Chooses snapshot of VM selected by user
 {
-    # Write-Host $snapshot_name
+    # Gets VM user selected
     $vm = Get-VM -Name $selected_vm.Name
     
+    # Error Handling
     try {
+
         # Prompt User for name of snapshot
         $snapPrompt = Read-Host -Prompt "Enter snapshot name (default: $snapshot_name)" 
+
         # Take snapshot of VM, terminate on error
         $snapshot = Get-Snapshot -VM $vm -Name $snapPrompt -ErrorAction Stop
         return $snapshot
     }
     catch {
+
+        # Prompts user if invalid input detected
         $uInput = Read-Host "No valid VM selected. Use default? (y/n)"
+
+        # Uses default, else quits
         if ($uInput -eq "y" -or  "Y") 
         {
             # Take snapshot of VM
@@ -105,16 +135,23 @@ function vmSnapshot ([string] $snapshot_name, $selected_vm)
 }
 
 function esxiHost ([string] $hostIP)
+# user selects ESXi host
 {
+    # Error Handling
     try {
         # Prompt user for IP of vmHost
         $hostPrompt = Read-Host -Prompt "Enter IP of VM Host (default: $hostIP)"
-        # Define vmHost
+
+        # Define vmHost, terminate on error
         $vmHost = Get-VMHost -Name $hostPrompt -ErrorAction Stop
         return $vmHost
     }
     catch {
+
+        # Runs if invalid input detected
         $uInput = Read-Host "No valid VM Host selected. Use default? (y/n)"
+
+        # uses default, else quits
         if ($uInput -eq "y" -or  "Y") 
         {
             # Define vmHost
@@ -128,16 +165,22 @@ function esxiHost ([string] $hostIP)
 }
 
 function datastoreName ([string] $datastore)
+# User selects datastore from ESXi host
 {
+    # Error Handling
     try {
         # Prompt user for datastore
-        $hostPrompt = Read-Host -Prompt "Enter IP of the datastore you would like to use (default: $datastore)"
+        $hostPrompt = Read-Host -Prompt "Enter the datastore you would like to use (default: $datastore)"
         # Define datastore
         $ds = Get-Datastore -Name $hostPrompt -ErrorAction Stop
         return $ds
     }
     catch {
+
+        # Runs if invalid input detected
         $uInput = Read-Host "No valid datastore selected. Use default? (y/n)"
+
+        # Uses default, else quits
         if ($uInput -eq "y" -or  "Y") 
         {
             # Define datastore
@@ -151,14 +194,17 @@ function datastoreName ([string] $datastore)
 }
 
 function linkedClone ($vm, $snapshot, $vmhost, $ds, $net)
+# Creates linked clone from predefined parameters and sets network adapter
 {
     Write-Host "Creating linked clone..."
+
     # Defines linked clone
     $linkedClone = "{0}.linked" -f $vm.name
 
     # Defines Linked VM
     $linkedVM = New-VM -LinkedClone -Name $linkedClone -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $ds
     
+    # Setting network adapter and Error handling
     try
     {
         # Promts user for network adapter name:
@@ -169,12 +215,16 @@ function linkedClone ($vm, $snapshot, $vmhost, $ds, $net)
     }
     catch
     {
+        # Runs if no valid input detected
         $uInput = Read-Host "No valid network adapter selected. Use default? (y/n)"
+
+        # Uses default, else quits
         if ($uInput -eq "y" -or  "Y") 
         {
             # Define datastore
             $linkedVM | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName $net
             Write-Host "Network adapter assigned: $net" -ForegroundColor Green
+            
         }else {
             Write-Host "Goodbye"
             exit
